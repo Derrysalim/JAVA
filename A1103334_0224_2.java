5d4c3f8c2eb8cb21c0839d69030722ca61bb5ea2
Derrysalim <125532064+Derrysalim@users.noreply.github.com>
2023-03-01 14:09:56 +0800

    Add files via upload

---
+++ A1103334_0224_2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class A1103334_0224_2{
+    public static void main(String[] argv){
+        int celcius, fahrenheit, q;
+
+        System.out.println("input celcius:");
+        Scanner sc = new Scanner(System.in);
+        celcius = sc.nextInt();
+
+        q = (celcius *9/5) + 32;
+        System.out.println(celcius+ " celcius is " + q+ " fahrenheit");
+    }
+}
